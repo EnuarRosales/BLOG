bc8bd46d14c86e6ce134175b545f62338766e106
enuar rosales <enuaremiliorosales@gmail.com>
2023-04-30 13:47:07 -0500

    se logra realizar la verificacion  a traves de correo

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.index');
-})->middleware(['auth']);
+    return view('welcome');  //admin.index
+}); //->middleware(['auth'])
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';

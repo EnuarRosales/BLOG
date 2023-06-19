40174f82c9ac1a1e94a44ca590362329dde11687
Oscar Sabogal <oscarsabogal285@gmail.com>
2023-06-19 18:44:36 -0500

    feat(backend): add error control in EmpresaController, and config logging

--- app/Http/Controllers/Admin/EmpresaController.php
+++ app/Http/Controllers/Admin/EmpresaController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Empresa;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
@@ -17,29 +19,40 @@ class EmpresaController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        $empresas = Empresa::all();
-        return view('admin.empresa.index', compact('empresas'));
+        try {
+            $empresas = Empresa::all();
+            return view('admin.empresa.index', compact('empresas'));
+        } catch (\Exception $exception) {
+            Log::error("Error EmpCr index: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
+            return back();
+        }
     }
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
-        return view('admin.empresa.create');
+        try {
+            return view('admin.empresa.create');
+        } catch (\Exception $exception) {
+            Log::error("Error EmpCr create: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
+            return back();
+        }
     }
+
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         try {
             DB::beginTransaction();
@@ -53,31 +66,27 @@ class EmpresaController extends Controller
 
         } catch (\Exception $exception) {
             DB::rollBack();
-            Log::error("Error EmpCr: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
-            return back()->withInput($request->all());
+            Log::error("Error EmpCr store: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
+            return back();
         }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|RedirectResponse
      */
-    public function edit(Empresa $empresa)
+    public function edit (Empresa $empresa)
     {
-        return view('admin.empresa.edit', compact('empresa'));
+        try {
+
+            return view('admin.empresa.edit', compact('empresa'));
+
+        } catch (\Exception $exception) {
+            Log::error("Error EmpCr edit: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
+            return back();
+        }
     }
 
     /**
@@ -85,43 +94,55 @@ class EmpresaController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, Empresa $empresa)
     {
-        if(!$request->file('logo')) {
-            $empresa->update($request->all());
-        } else {
-            $request_data = $request->all();
+        try {
 
-            $name_logo = uniqid($empresa->id . '-').'.'.$request->file('logo')->getClientOriginalExtension();
+            DB::beginTransaction();
 
-            Storage::disk('public-logo')->put($name_logo, File::get($request->file('logo')));
+            if(!$request->file('logo')) {
+                $empresa->update($request->all());
+            } else {
+                $request_data = $request->all();
 
-            $url_logo = Storage::disk('public-logo')->url($name_logo);
+                $name_logo = uniqid($empresa->id . '-').'.'.$request->file('logo')->getClientOriginalExtension();
 
-            $request_data['logo'] = $url_logo;
+                Storage::disk('public-logo')->put($name_logo, File::get($request->file('logo')));
 
-            $empresa->update($request_data);
+                $url_logo = Storage::disk('public-logo')->url($name_logo);
+
+                $request_data['logo'] = $url_logo;
+
+                $empresa->update($request_data);
+            }
+            DB::commit();
+            return redirect()->route('admin.empresa.index')->with('info', 'update');
+        }catch (\Exception $exception) {
+            DB::rollBack();
+            Log::error("Error EmpCr update: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
+            return back();
         }
-
-        return redirect()->route('admin.empresa.index')->with('info', 'update');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy(Empresa $empresa)
     {
         try {
-
+            DB::beginTransaction();
             $empresa->delete();
+            DB::commit();
             return redirect()->route('admin.empresa.index')->with('info', 'delete');
         } catch (\Exception $exception) {
-
+            DB::rollBack();
+            Log::error("Error EmpCr destroy: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
+            return back();
         }
     }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -70,9 +70,6 @@
 @stop
 
 @section('js')
-    <script>
-        console.log('Hi!');
-    </script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>

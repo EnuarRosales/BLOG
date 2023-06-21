e582529ed12c0426191dab706c25de45af470b1d
enuar rosales <enuaremiliorosales@gmail.com>
2023-06-21 00:13:17 -0500

    SE AGREGAR VALIDADCIONES AL EXCEL QUE SE DESEA SUBIR

--- app/Imports/ReportePaginasImport.php
+++ app/Imports/ReportePaginasImport.php
@@ -2,12 +2,25 @@
 
 namespace App\Imports;
 
+use App\Models\Pagina;
 use App\Models\ReportePagina;
+
+
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class ReportePaginasImport implements ToModel, WithHeadingRow
+class ReportePaginasImport implements ToModel, WithHeadingRow, WithValidation
+
 {
+    // CON ESTA VARIABLE Y CON EL CONSTRUCTOR LOGRAMOS PASAR DEL NOMBRE AL ID, YA QUE LA TABLA RECIBE UN ID
+    private $pagina;
+
+    public function __construct()
+    {
+        $this->pagina = Pagina::pluck('id', 'nombre');
+    }
+
     /**
      * @param array $row
      *
@@ -21,10 +34,47 @@ class ReportePaginasImport implements ToModel, WithHeadingRow
             //IZQUIERDA ATRIBUTOS BD DERECHA DATOS EXCEL
             'fecha' => $row['fecha'],
             'Cantidad' => $row['cantidad'],
-            'user_id' => 51,
-            'pagina_id' => 1,
+            'user_id' => $row['modelo'],
+            'pagina_id' => $this->pagina[$row['pagina']]
             
             //
         ]);
     }
+
+
+    // CON ESTO VALIDAMOS LAS INFORMACION OJO QUE VA LA INFO DEl  EXCEL
+    public function rules(): array
+    {
+        return [
+            'cantidad' => [
+
+                'regex:/^[0-9]+(\.[0-9]{1,2})?$/', //  CON ESTO VALIDAMOS QUE EL VALOR SEA DECIMAL
+                'required'
+            ],
+
+            'modelo' => [
+
+                //'numeric', //  CON ESTO VALIDAMOS QUE EL VALOR SEA DECIMAL
+                'integer',
+                'required'
+            ],
+
+            'fecha' => [
+
+                'date', //  CON ESTO VALIDAMOS QUE EL VALOR SEA DECIMAL
+                'required'
+            ],
+
+            'pagina' => [
+
+                'string', //  CON ESTO VALIDAMOS QUE EL VALOR SEA DECIMAL
+                'required'
+            ],
+
+
+             
+        ];
+    }
+
 }
+

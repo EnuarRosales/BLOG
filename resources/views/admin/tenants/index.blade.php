ee0e7e00a412efce9f076fb3205ccc56981a7375
erwilgomez14 <erwildiaz@gmail.com>
2023-09-21 09:05:09 -0400

    terminando la migracion

--- resources/views/admin/tenants/index.blade.php
+++ resources/views/admin/tenants/index.blade.php
@@ -12,7 +12,7 @@
 
 
 @section('content_header')
-    <h2>Listado de Estudios</h2>
+    <h2 class="ml-3">Listado de Estudios</h2>
 
 
 @stop
@@ -30,16 +30,29 @@
         <div class="widget-content widget-content-area br-6">
             <div class="row g-2">
                 <div class="col">
-                    @yield('content_header')
-                </div>
-                <div class="col">
                     @can('admin.tenants.create')
-                        <a class="btn btn-primary float-right" href="{{ route('admin.tenants.create') }}">Agregar Inquilino</a>
+                        <a class="btn btn-primary float-right mr-4" href="{{ route('admin.tenants.create') }}">Agregar
+                            Inquilino</a>
                     @endcan
                 </div>
             </div>
             <div class="table-responsive mb-4 mt-4">
                 <table id="html5-extension" class="table table-hover non-hover" style="width:100%">
+                    <div class="row">
+                        <div class="col-md-6 d-flex align-items-center ml-3">
+                            <label class="mb-0 mr-2">Mostrar:</label>
+                            <select id="records-per-page" class="form-control form-control-sm custom-width-20">
+                                <!-- Agregamos la clase form-control-sm -->
+                                <option value="7">7</option>
+                                <option value="10">10</option>
+                                <option value="20">20</option>
+                                <option value="50">50</option>
+                            </select>
+                            <span class="ml-2">registros por página</span>
+                            <!-- Agregamos un espacio después del select -->
+                        </div>
+
+                    </div>
                     <thead>
                         <tr>
                             <th>Id</th>
@@ -141,35 +154,107 @@
     <script src="{{ asset('template/plugins/table/datatable/button-ext/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('template/plugins/table/datatable/button-ext/buttons.print.min.js') }}"></script>
     <script>
-        $('#html5-extension').DataTable( {
+        var table = $('#html5-extension').DataTable({
             dom: '<"row"<"col-md-12"<"row"<"col-md-6"B><"col-md-6"f> > ><"col-md-12"rt> <"col-md-12"<"row"<"col-md-5"i><"col-md-7"p>>> >',
             buttons: {
-                buttons: [
-                    { extend: 'copy', className: 'btn' },
-                    { extend: 'csv', className: 'btn' },
-                    { extend: 'excel', className: 'btn' },
-                    { extend: 'print', className: 'btn' }
+                buttons: [{
+                        extend: 'copy',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'csv',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'excel',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'print',
+                        className: 'btn'
+                    }
                 ]
             },
             "oLanguage": {
-                "oPaginate": { "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>', "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>' },
+                "oPaginate": {
+                    "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+                    "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
+                },
                 "sInfo": "Página _PAGE_ de _PAGES_",
                 "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
                 "sSearchPlaceholder": "Buscar...",
-               "sLengthMenu": "Results :  _MENU_",
+                "sLengthMenu": "Mostrar _MENU_ resultados por página",
             },
             "stripeClasses": [],
             "lengthMenu": [7, 10, 20, 50],
             "pageLength": 7
-        } );
+        });
+
+        // Vincular eventos de clic para eliminar
+        function bindDeleteEvents() {
+            document.querySelectorAll('.eliminar-registro').forEach(botonEliminar => {
+                botonEliminar.addEventListener('click', function(e) {
+                    e.preventDefault();
+
+                    const rolId = this.getAttribute('data-rol-id');
+
+
+                    Swal.fire({
+                        title: '¿Estás seguro?',
+                        text: '¡Este registro se eliminará definitivamente!',
+                        type: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: '¡Sí, eliminar!',
+                        cancelButtonText: '¡Cancelar!',
+                        preConfirm: () => {
+                            // Crear un formulario dinámicamente
+                            const formulario = document.createElement('form');
+                            formulario.action =
+                                `admin/tenants/${rolId}`; // Ruta de eliminación
+                            formulario.method = 'POST'; // Método POST
+                            formulario.style.display = 'none'; // Ocultar el formulario
+
+                            // Agregar el token CSRF al formulario
+                            const tokenField = document.createElement('input');
+                            tokenField.type = 'hidden';
+                            tokenField.name = '_token';
+                            tokenField.value = '{{ csrf_token() }}';
+                            formulario.appendChild(tokenField);
+
+                            // Agregar un campo oculto para indicar que es una solicitud de eliminación
+                            const methodField = document.createElement('input');
+                            methodField.type = 'hidden';
+                            methodField.name = '_method';
+                            methodField.value = 'DELETE';
+                            formulario.appendChild(methodField);
+
+                            // Adjuntar el formulario al documento y enviarlo
+                            document.body.appendChild(formulario);
+                            formulario.submit();
+
+                            return true;
+                        },
+                    });
+                });
+            });
+        }
+
+        // Volver a vincular eventos de clic después de cada redibujo
+        table.on('draw.dt', function() {
+            bindDeleteEvents();
+        });
+
+        // Detectar cambios en el select
+        $('#records-per-page').change(function() {
+            var newLength = $(this).val();
+            table.page.len(newLength).draw();
+        });
+
+        // Vincular eventos de clic para eliminar inicialmente
+        bindDeleteEvents();
     </script>
-    <script>
-        console.log('Hi!');
-    </script>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
 
@@ -186,7 +271,7 @@
         <script>
             Swal.fire({
                 position: 'top-end',
-                icon: 'success',
+                type: 'success',
                 title: 'Descuento registrado correctamente',
                 showConfirmButton: false,
                 timer: 2000
@@ -196,7 +281,7 @@
         <script>
             Swal.fire({
                 position: 'top-end',
-                icon: 'warning',
+                type: 'warning',
                 title: 'No hay saldo que descontar',
                 showConfirmButton: false,
                 timer: 2000
@@ -206,7 +291,7 @@
         <script>
             Swal.fire({
                 position: 'top-end',
-                icon: 'success',
+                type: 'success',
                 title: 'Descuento correctamente',
                 showConfirmButton: false,
                 timer: 2000
@@ -214,51 +299,9 @@
         </script>
     @endif
 
-    <script>
-        $('.formulario-eliminar').submit(function(e) {
-            e.preventDefault();
-
-            Swal.fire({
-                title: '¿Estas Seguro?',
-                text: "¡Este registro se eliminara definitivamente!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: '¡Si, eliminar!',
-                cancelButtonText: '¡Cancelar!',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    this.submit();
-                }
-            })
-
-        })
-    </script>
 
 
-    {{-- DATATATABLE --}}
 
-    <script>
-        $(document).ready(function() {
-            $('#registroDescuentos').DataTable({
-                dom: 'Blfrtip',
 
-                buttons: [
-                    'copy', 'csv', 'excel', 'pdf', 'print'
-                ]
-            });
-
-        });
-    </script>
-
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
 
 @stop

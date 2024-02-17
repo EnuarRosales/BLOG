76a5012c29a5a1ea4a5aa2b05b8bdbeade0b4bab
HenryMJL <henrymauro.j@hotmail.com>
2024-02-17 12:51:18 -0500

    update deshabilitacion de usuarios

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Events\userModelEvent;
 use App\Events\usuarios_widget;
 use App\Http\Controllers\Controller;
+use App\Models\AsignacionRoom;
+use App\Models\AsignacionTurno;
 use App\Models\Empresa;
 use App\Models\Pago;
 use App\Models\TipoUsuario;
@@ -17,7 +18,6 @@ use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Database\QueryException;
-use PhpParser\Node\Stmt\TryCatch;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class UserController extends Controller
@@ -72,7 +72,6 @@ class UserController extends Controller
                 $month[] = $cantidadAno->m;
                 $day[] = $cantidadAno->d;
             }
-
             $i = 0;
             return view('admin.users.certificacionTiempo', compact('userLogueado', 'users', 'year', 'month', 'day', 'i'));
         } catch (\Exception $exception) {
@@ -99,17 +98,6 @@ class UserController extends Controller
         $cantidadMes = $fechaAntigua->diffInMonths($fechaReciente);
         $cantidadAno = $fechaAntigua->diff($fechaReciente);
         $ano = $cantidadAno->m;
-
-        // $codigoQR = QrCode::size(80)->generate(
-        //     "CERTIFICACION LABORAL" . "\n" .
-        //         "NOMBRE: " . $user->name . "\n" .
-        //         "LABORANDO: " . "\n" .
-        //         "EMPRESA: " . $empresa->name . "\n" .
-        //         "DESDE: " . $user->fechaIngreso . "\n" .
-        //         "HASTA: " . $fechaReciente . "\n"
-
-        // );
-
         try {
             $codigoQR = QrCode::size(80)->generate(
                 "CERTIFICACION LABORAL" . "\n" .
@@ -124,16 +112,7 @@ class UserController extends Controller
             $message = substr($errorMessage, strpos($errorMessage, '$') + 1);
             return back()->with('mensaje', "Falta Informacion sobre: " . $message);
         }
-
-
-
-        // printf('%d años, %d meses, %d días, %d horas, %d minutos', $cantidadAno->y, $cantidadAno->m, $cantidadAno->d, $cantidadAno->h, $cantidadAno->i);
-        // dd($logoEmpresa);
-        // try {
         $pdf = PDF::loadView('admin.users.laboralPDF', compact('user', 'date', 'nombreEmpresa', 'nitEmpresa', 'gerenteEmpresa', 'fechaAntigua', 'cantidadDias', 'cantidadMes', 'cantidadAno', 'codigoQR', 'logoEmpresa'));
-        // } catch (\Exception $e) {
-        //     return back()->with('mensaje', 'Error al generar el PDF: ' . $e->getMessage());
-        // }
         return $pdf->stream();
     }
 
@@ -143,18 +122,11 @@ class UserController extends Controller
         $fechaReciente = Carbon::now();
         $empresas = Empresa::all();
         foreach ($empresas as $empresa) {
-            // dd($empresa);
             $nombreEmpresa = $empresa->name;
             $nitEmpresa = $empresa->nit;
             $gerenteEmpresa = $empresa->representative;
             $logoEmpresa = $empresa->logo;
         }
-        // if (empty($nombreEmpresa)) {
-        //     //  return back()->with('preuba', 'updateRol');
-        //     return redirect()->back()->withErrors(['mensaje' => 'Ocurrió un error.']);
-
-
-        // }
         $fechaAntigua1 = Carbon::parse($user->fechaIngreso);
         $fechaAntigua = $fechaAntigua1->locale('es');
         $cantidadDias = $fechaAntigua->diffInDays($fechaReciente);
@@ -176,9 +148,6 @@ class UserController extends Controller
 
         return $pdf->stream();
     }
-
-
-
 
     /**
      * Show the form for creating a new resource.
@@ -203,7 +172,6 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-
         //VALiDACION FORMULARIO
         $request->validate([
             'fechaIngreso' => 'required',
@@ -244,43 +212,7 @@ class UserController extends Controller
             Log::error("Error UC store: {$e->getMessage()}, File: {$e->getFile()}, Line: {$e->getLine()}");
             return back()->withInput()->withErrors(['general' => 'Hubo un problema al procesar la solicitud. Por favor, inténtelo de nuevo.']);
         }
-
-        // try {
-        //     $user = User::create($request->all());
-        //     $nameTipoUsuario = TipoUsuario::where('id', $user->tipoUsuario_id)->first();
-        //     $idRole = Role::where('name', $nameTipoUsuario->nombre)->first();
-
-        //     if ($idRole) {
-        //         try {
-        //             DB::beginTransaction();
-        //             $user->roles()->sync($idRole->id);
-        //             DB::commit();
-        //             // return redirect()->route('admin.users.index')->with('info', 'updateRol'); //with mensaje de sesion
-        //         } catch (\Exception $exception) {
-        //             DB::rollBack();
-        //             Log::error("Error UC updateRol: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
-        //         }
-        //     }
-        //     event(new usuarios_widget);
-        // } catch (QueryException $e) {
-        //     // Verificar si la excepción es por violación de clave única (correo electrónico duplicado)
-        //     if ($e->errorInfo[1] == 1062) {
-        //         // El código 1062 es específico de MySQL para violación de clave única
-        //         return back()->withInput()->withErrors(['email' => 'El correo electrónico ya está registrado. Por favor, elija otro.']);
-        //     } else {
-        //         // Si la excepción no es por violación de clave única, puedes manejarla según tus necesidades
-        //         return back()->withInput()->withErrors(['general' => 'Hubo un problema al procesar la solicitud. Por favor, inténtelo de nuevo.']);
-        //     }
-        // }
-        // return redirect()->route('admin.users.index', $user->id)->with('info', 'store');
     }
-
-
-
-
-
-
-
 
     /**
      * Show the form for editing the specified resource.
@@ -294,7 +226,6 @@ class UserController extends Controller
         $empresas = Empresa::orderBy('id', 'desc');
         return view('admin.users.edit', compact('user', 'tipoUsuarios', 'empresas'));
     }
-
 
     public function rol(User $user)
     {
@@ -313,48 +244,6 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-
-
-
-    // public function update(Request $request, User $user)
-    // {
-    //     //VALiDACION FORMULARIO
-    //     try {
-    //         DB::beginTransaction();
-    //         $request->validate([
-    //             'fechaIngreso' => 'required',
-    //             'name' => 'required',
-    //             'cedula' => 'required',
-    //             'celular' => 'required',
-    //             'direccion' => 'required',
-    //             'email' => 'required',
-    //             // 'tipoUsuario_id' => 'required',
-    //         ]);
-
-    //         if ($request->input('empresa_id')) {
-    //             $empresa = Empresa::find($request->input('empresa_id'));
-    //             if (!$empresa->users()->where('user_id', $user->id)->first())
-    //                 $empresa->users()->attach($user->id);
-    //         }
-
-    //         $request = $request->except('empresa_id');
-    //         //ASINACION MASIVA DE VARIABLES A LOS CAMPOS
-    //         $user->update($request);
-    //         DB::commit();
-
-    //         if ($user->active) {
-    //             userModelEvent::dispatch($user->id);
-    //         }
-
-    //         return redirect()->route('admin.users.index', $user->id)->with('info', 'update'); //with mensaje de sesion
-
-    //     } catch (\Exception $exception) {
-    //         DB::rollBack();
-    //         Log::error("Error UC update: {$exception->getMessage()}, File: {$exception->getFile()}, Line: {$exception->getLine()}");
-    //     }
-    // }
-
-
     public function update(Request $request, User $user)
     {
         //VALiDACION FORMULARIO
@@ -368,18 +257,18 @@ class UserController extends Controller
             'tipoUsuario_id' => 'required',
             'empresa_id' => 'required',
         ]);
-
+        if ($request->active == 0) {
+            $turnos = AsignacionTurno::where('user_id', $user->id)->get();
+            $rooms = AsignacionRoom::where('user_id', $user->id)->get();
+            $allModels = $turnos->concat($rooms);
+            $allModels->each(function ($model) {
+                $model->update(['deleted_at' => Carbon::now()]);
+            });
+        }
 
         $user->update($request->all());
         return redirect()->route('admin.users.index', $user->id)->with('info', 'update'); //with mensaje de sesion
     }
-
-
-
-
-
-
-
 
     public function updateRol(Request $request, User $user)
     {
@@ -394,9 +283,6 @@ class UserController extends Controller
         }
     }
 
-
-
-
     /**
      * Remove the specified resource from storage.
      *
@@ -405,7 +291,6 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        // dd($user);
         try {
             DB::beginTransaction();
             $user->delete();
@@ -417,7 +302,6 @@ class UserController extends Controller
         }
     }
 
-
     //METODOS ROSALES PARA LAS FUNCIONADLIDADES
     public function calcularPorcentajePersonal()
     {
@@ -425,8 +309,6 @@ class UserController extends Controller
         $user = DB::table('user')->count();
         return "el resultado es " . $user;
     }
-
-
 
     public function comprobantePagoPDF(Pago $pago)
     {

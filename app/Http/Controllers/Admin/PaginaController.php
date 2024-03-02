76fb0a74af9c0620d812c891ae0205f31474db62
HenryMJL <henrymauro.j@hotmail.com>
2024-03-01 22:26:35 -0500

    update

--- app/Http/Controllers/Admin/PaginaController.php
+++ app/Http/Controllers/Admin/PaginaController.php
@@ -16,9 +16,9 @@ class PaginaController extends Controller
      */
     public function index()
     {
-        $paginas = Pagina::orderBy('id','desc')->paginate(); 
+        $paginas = Pagina::orderBy('id','desc')->paginate();
         return view('admin.paginas.index',compact('paginas'));
-        // $paginas = Pagina::all();        
+        // $paginas = Pagina::all();
         // return $paginas;
     }
 
@@ -29,7 +29,7 @@ class PaginaController extends Controller
      */
     public function create()
     {
-        // $tipoMonedapaginas = TipoMonedaPagina::orderBy('id','desc'); 
+        // $tipoMonedapaginas = TipoMonedaPagina::orderBy('id','desc');
         return view('admin.paginas.create');
     }
 
@@ -41,13 +41,13 @@ class PaginaController extends Controller
      */
     public function store(Request $request)
     {
-        //VALiDACION FORMULARIO 
+        //VALiDACION FORMULARIO
        $request->validate([
         'nombre'=>'required',
-        'valor'=>'required',
+        'valor' => ['required', 'regex:/^\d+(\.\d{1,2})?$/'], 
         'moneda'=>'required',
-              
-    ]); 
+
+    ]);
     $pagina= Pagina::create($request->all());
     return redirect()->route('admin.paginas.index',$pagina->id)->with('info','store');
 
@@ -72,10 +72,10 @@ class PaginaController extends Controller
      */
     public function edit(Pagina $pagina)
     {
-        // $tipoMonedapaginas = TipoMonedaPagina::orderBy('id','desc'); 
+        // $tipoMonedapaginas = TipoMonedaPagina::orderBy('id','desc');
         // return $pagina;
         return view('admin.paginas.edit',compact('pagina'));
-    } 
+    }
 
     /**
      * Update the specified resource in storage.
@@ -89,14 +89,14 @@ class PaginaController extends Controller
         //VALLIDACION DE FORMULARIOS
         $request->validate([
             'nombre'=>'required',
-            'valor'=>'required', 
+            'valor'=>'required',
             'moneda'=>'required',
-            
+
         ]);
         //ASINACION MASIVA DE VARIABLES A LOS CAMPOS
-        $pagina->update($request->all());           
+        $pagina->update($request->all());
         return redirect()->route('admin.paginas.index',$pagina->id)->with('info','update');//with mensaje de sesion
-              
+
     }
 
     /**
